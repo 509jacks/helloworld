2f507f14f73975d7e70548a17797210a32aa9afa
509jacks <122555395+509jacks@users.noreply.github.com>
2023-01-13 12:25:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showString("I'm your mom")
     basic.showIcon(IconNames.Duck)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
+    basic.showString("I'm your dad")
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        `)
 })
+basic.showString("Avanti is suss")
 basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playMelody("E F C5 E D A D E ", 500)
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Forever)
+})
